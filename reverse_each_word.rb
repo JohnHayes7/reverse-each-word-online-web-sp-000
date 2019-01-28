96925de426975e9c68be48300fc6abeb3c0cb829
John Hayes <johnnyhayes7@gmail.com>
2019-01-28 00:09:11 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(sentence)
+  rev_array = sentence.split
+  new_array = []
+  rev_array.each do |word|
+    new_array << word.reverse
+  end
+  return new_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  rev_array = sentence.split
+    rev_array.map do |word|
+     rev_array << word.reverse
+  end
+  return rev_array
+end
